c5dd1b0f89615018c373b4228ad26b799098bd3c
davmz <74930417+davmz@users.noreply.github.com>
2021-07-03 17:40:35 -0500

    Menu Bar Navigation
    working for mobile nav

--- script.js
+++ script.js
@@ -9,15 +9,33 @@ document.addEventListener("DOMContentLoaded", load);
 
 function load()
 {
-    const navSlide = () =>
+    function navSlide()
     {
-        let burger = document.querySelector(".burger");
-        let nav = document.querySelector(".navList");
+        const menuBtn = document.querySelector(".menuBtn");
+        const menuItems = document.querySelector("#navList");
+        const menuItem = document.querySelectorAll(".navItem")
 
-        burger.addEventListener("click", () =>
+        menuBtn.addEventListener("click", () =>
         {
-            nav.classList.toggle("navActive");
+            toggle();
         });
+
+        menuItem.forEach(item =>
+            {
+                item.addEventListener("click", () =>
+                {
+                    if(menuBtn.classList.contains("open"))
+                    {
+                        toggle();
+                    }
+                });  
+            });
+
+        function toggle()
+        {
+            menuBtn.classList.toggle("open");
+            menuItems.classList.toggle("open");
+        }
     }
 
     navSlide();
