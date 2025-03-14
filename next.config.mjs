f09302062d106c443ca3b06c7d843ed160b49299
David <montanezdavid@gmail.com>
2025-03-13 20:04:45 -0500

    Fix: hopefully pushes right url for tailwindcss support

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
+    basePath: "/davmz.github.io"
+    // assetPrefix: isProd ? "https://davmz.github.io" : "", // ✅ Serve assets from GitHub Pages, NOT GitHub Repo
+    // basePath: isProd ? "/davmz.github.io" : "", // ✅ Ensures correct paths for pages
+};
 
 export default nextConfig;
